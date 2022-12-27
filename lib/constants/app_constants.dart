809aa94f57a2abb05576561af89d5b2932b5b09e
Fakhar <fakhar.oraxtech@gmail.com>
2022-12-27 19:17:14 +0500

    Text Media screen : added -> added a button to listen voice input

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -66,5 +66,7 @@ class AppConstants {
   //Text media screen
   static const String textTextMedia = "Text Media";
   static const String textWriteHere = "Write here...";
+    static const String textMicCantBeUsed = "Mic cannot be used";
+
 
 }
